a06694150bfd22a8a31a8a4458f2266f2db863f7
Brenox889 <brenomendes889@hotmail.com>
2020-11-21 23:45:08 -0300

    feat: react icons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 import logo from '../../assets/logoGit.svg';
+import Repository from '../Repository';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +16,20 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite aqui" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/61329950?s=460&u=b1d454fdc4c0abdaaf9569866607b14a6a7e6677&v=4"
+            alt="Breno Mendes"
+          />
+          <div>
+            <strong>GoBarber</strong>
+            <p>Lorem Ipsum é nois</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
